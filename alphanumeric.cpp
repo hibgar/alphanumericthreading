be50d6c31e6605f8d382df6febd30bdf28a9e194
Hiba Mughal <hibgar@gmail.com>
2023-11-26 11:57:20 -0800

    working code

--- alphanumeric.cpp
+++ alphanumeric.cpp
@@ -9,86 +9,60 @@ int iter = 0;
 string inputString;
 bool switchn = false;
 bool switcha = false;
-/*
+
 void* alpha_func(void* arg) {
+
     while (iter < inputString.size()) {
-        if (isalpha(inputString[iter])) {
-            cout << "alpha: ";
-            while (inputString[iter] != ' ' && inputString[iter] != '\0') {
-                cout << inputString[iter];
+        if (switcha) {
+            if (isalpha(inputString[iter])){
+                cout << "alpha: ";
+                while (inputString[iter] != ' ' && inputString[iter] != '\0') {
+                    cout << inputString[iter];
+                    iter++;
+                }
+                cout << endl;
                 iter++;
             }
-            cout << endl;
-        } else {
-            switchn = true;
-            break;
+            
+            else {
+                if (isdigit(inputString[iter])) {
+                    switcha = false;
+                    switchn = true;
+                }
+                
+            }
+            
         }
-        iter++;
+        
     }
     pthread_exit(0);
 }
 
 void* numeric_func(void* arg) {
+    
     while (iter < inputString.size()) {
-        if (isdigit(inputString[iter])) {
-            cout << "numeric: ";
-            while (inputString[iter] != ' ' && inputString[iter] != '\0') {
-                cout << inputString[iter];
-                iter++;
-            }
-            cout << endl;
-        } else {
-            switchn = false;
-            if (switcha) {
-                switcha = false;
-                break;
-            }
-            switcha = true;
-        }
-        iter++;
-    }
-    pthread_exit(0);
-}
-*/
-// Both alpha and numeric get the whole string, I didn't add break just made it so if not bool or bool depending then print otherwise it doesn't do anything and kinda gets stuck at that index until the other thread changes it
-// No breaking it will get stuck at index index should be global anfld manually updated in the if statements
+        if (switchn) {
 
-void* alpha_func(void* arg) {
-    while (iter < inputString.size()) {
-        if (isalpha(inputString[iter])) {
-            cout << "alpha: ";
-            while (inputString[iter] != ' ' && inputString[iter] != '\0') {
-                cout << inputString[iter];
+            if (isdigit(inputString[iter])) {
+                cout << "numeric: ";
+                while (inputString[iter] != ' ' && inputString[iter] != '\0') {
+                    cout << inputString[iter];
+                    iter++;
+                }
+                cout << endl;
                 iter++;
             }
-            cout << endl;
-        } else {
-            switchn = true;
-            break;
-        }
-        iter++;
-    }
-    pthread_exit(0);
-}
-
-void* numeric_func(void* arg) {
-    while (iter < inputString.size()) {
-        if (isdigit(inputString[iter])) {
-            cout << "numeric: ";
-            while (inputString[iter] != ' ' && inputString[iter] != '\0') {
-                cout << inputString[iter];
-                iter++;
+            
+            else {
+                if (isalpha(inputString[iter])){
+                    switchn = false;
+                    switcha = true;
+                }
+                
             }
-            cout << endl;
-        } else {
-            switchn = false;
-            if (switcha) {
-                switcha = false;
-                break;
-            }
-            switcha = true;
+            
         }
-        iter++;
+        
     }
     pthread_exit(0);
 }
